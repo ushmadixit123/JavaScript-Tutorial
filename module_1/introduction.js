90a8974a36ae6d8d67ee0003be52a18fb81f8798
ushmadixit123 <ushmadixit@gmail.com>
2024-05-20 20:19:53 +0530

    Introduction to javascript

---
+++ module_1/introduction.js
@@ -0,0 +1,9 @@
+// What is Javascript ?
+// JavaScript is a High level , Multi-paradigm , object-oriented programming language.
+// It is also synchonous and single threaded language.
+// It is language of web and can be used for wepages and web Application work dynamically.
+
+
+// Javascript Engine :- is a software responsible for running javascript on web browser
+// chrome uses V8 engine and firefox uses spider monkey
+
