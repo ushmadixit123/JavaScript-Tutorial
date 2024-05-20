e4f914c5696874cc48e8a82c37a4588e2e3fa461
ushmadixit123 <ushmadixit@gmail.com>
2024-05-20 21:57:19 +0530

    basic functions in js

---
+++ module_4/basicFunction.js
@@ -0,0 +1,20 @@
+// Functions : - A function is a block of code that performs a specific task.
+
+function greet(){
+    console.log("Hello");
+}
+greet();
+
+//  parameters :-
+
+// Arguments :-
+
+// function expression 
+
+var add = function(a,b){
+    return a+b;
+}
+console.log(add(2,3));
+
+// Hoisting :- is a concept in which Js allows to move declarations on top
+// it is done only in normal functions not in function expression and variable declaration.
